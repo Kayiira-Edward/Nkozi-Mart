f4a8d1146c716c09321cf8a2fec0d242675a5282
Kayiira-Edward <96061154+Kayiira-Edward@users.noreply.github.com>
2025-08-15 11:19:19 +0300

    icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,27 +1,4 @@
-// import "./globals.css";
-// import "../fontawesome";
-// import { GeistSans } from "geist/font/sans"; // Correct import for Geist Sans
-// import { GeistMono } from "geist/font/mono"; // Correct import for Geist Mono
-// import CartProvider from "./providers/CartProvider";
-// import Footer from "./components/Footer";
 
-// export const metadata = {
-//   title: "Nkozi Mart - Marketplace",
-//   description: "Buy and sell anything easily.",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>
-//         <CartProvider>
-//           <main>{children}</main>
-//         </CartProvider>
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
 import "./globals.css";
 import "../fontawesome";
 import { GeistSans } from "geist/font/sans";
